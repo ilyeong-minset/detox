d99aaa88a463fe74b2f5a59546d9d9d731f08cdd
inmoonlight <mjihyung@gmail.com>
2020-08-20 15:59:04 +0900

    Lint

--- convert_to_kaggle_submission.py
+++ convert_to_kaggle_submission.py
@@ -20,12 +20,16 @@ def main(result_path):
     result = pd.read_csv(result_path, sep="\t")
     result["label"] = result["prediction"].map(lambda x: label2idx[x])
     kaggle_output_filename = os.path.basename(result)
-    result[["comments", "label"]].to_csv(f"{kaggle_dir}/{kaggle_output_filename}.csv", index=False)
+    result[["comments", "label"]].to_csv(
+        f"{kaggle_dir}/{kaggle_output_filename}.csv", index=False
+    )
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--result-path", help="Path to a model prediction result", required=True)
+    parser.add_argument(
+        "--result-path", help="Path to a model prediction result", required=True
+    )
     args = parser.parse_args()
 
     main(args.result_path)
